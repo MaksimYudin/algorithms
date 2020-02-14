0079e9ac18fca708986ea2eecc623e804cd40719
Maksim Yudin <metalman@inbox.ru>
2020-02-14 17:21:12 +0300

    Memoization

--- src/Algs.java
+++ src/Algs.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Algs {
     public static void main(String[] args) {
+        int n = 50;
+        long[] mem = new long[n + 1];
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaiveMem(n, mem));
         //System.out.println(calcFibonachiRecursive(50));
-        System.out.println(calcFibonachi(50));
+        System.out.println(fibFast(50));
     }
 
     // Вычисление числа фибоначчм
-    private static long calcFibonachiRecursive(int n) {
+    private static long fibNaive(int n) {
         if (n <= 1)
             return n;
 
-        return calcFibonachiRecursive(n - 1) + calcFibonachiRecursive(n - 2);
+        return fibNaive(n - 1) + fibNaive(n - 2);
     }
     // 0, 1, 1, 2, 3, 5, 8, 13
-    private static long calcFibonachi(int n) {
+    private static long fibFast(int n) {
         List<Long> array = new ArrayList<>();
 
         array.add((long) 0);
@@ -27,5 +33,17 @@ public class Algs {
             array.add(value);
         }
         return array.get(n);
+    }
+
+    private static long fibNaiveMem(int n, long[] mem) {
+        if (mem[n] != -1)
+            return mem[n];
+
+        if (n <= 1)
+            return n;
+
+        long value = fibNaiveMem(n - 1, mem) + fibNaiveMem(n - 2, mem);
+        mem[n] = value;
+        return value;
     }
 }
